2f6c097fdbb1e99b995d63946e7201733fd452f5
Ricardo Ribeiro <ricardoribeiro.j7@gmail.com>
2024-01-11 15:30:09 -0300

    implementação do CRUD.

---
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -0,0 +1,10 @@
+package med.voll.api.medico;
+
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
+
+	//Mais um construtor que chama o construtor padrão do record
+	public DadosListagemMedico(Medico medico) {
+		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+	}
+	
+}
